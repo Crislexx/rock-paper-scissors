1a8d9c87ca94a0b2454295cbfc8d05b1b6d44867
Cristian Manole <cris.alex.manole@gmail.com>
2022-09-16 12:13:34 +0300

    Create function singleRound

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 function getComputerChoice(min=1, max=3){
     if (Math.floor(Math.random() * (max - min + 1) + min)===1){
-        return "Rock";
+        return "rock";
     }else if(Math.floor(Math.random() * (max - min + 1) + min)===2){
-        return "Paper";
+        return "paper";
     }else 
-        return "Scissors";
+        return "scissors";
 }
+
+function singleRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return`You both chose ${playerSelection}! it's a tie!`;
+    }else if(playerSelection == "rock" && computerSelection == "paper"){
+        return"You lose! paper beats rock";
+    }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return"You win! rock beats scissors";
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return"You win! paper beats rock";
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return"You lose! scissors beats paper";
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return"You lose! rock beats scissors";
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return"You win! scissors beats paper";
+    }
+        
+}
+
+const playerSelection = prompt("Please write your selection between: Rock, Paper and Scissors").toLocaleLowerCase();
+const computerSelection = getComputerChoice();
+console.log(singleRound(playerSelection, computerSelection));
